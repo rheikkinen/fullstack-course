bf709b5b6f095771513b6062f0e17fcb8bfd9652
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-09-28 19:18:53 +0300

    Render notification stored in Redux store
    Exercise 6.12

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: null,
+  reducers: {
+    setNotification: (state, action) => action.payload,
+  }
+})
+
+export const { setNotification } = notificationSlice.actions
+export default notificationSlice.reducer
