8f2358596098ec5e55e0c0a590170fc86da9850d
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-10-05 16:08:39 +0300

    Display info link on the anecdote page
    Exercise 7.2 finished

--- part7/routed-anecdotes/src/components/Anecdote.jsx
+++ part7/routed-anecdotes/src/components/Anecdote.jsx
@@ -10,6 +10,17 @@ const Anecdote = ({ anecdote }) => {
         </h3>
       }
       <p>has {anecdote.votes} votes</p>
+      {anecdote.info &&
+        <p>For more info see: <br />
+          <a href={anecdote.info.includes('//')
+            ? anecdote.info
+            : `//${anecdote.info}`}
+            target='_blank'
+            rel='noreferrer'>
+            {anecdote.info}
+          </a>
+        </p>
+      }
     </div>
   )
 }
